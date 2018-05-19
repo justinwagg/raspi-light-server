745c875f5f6e820fcd8f1fa0059e0d98799bc482
justinwagg <jmwagg@gmail.com>
2018-05-19 14:34:49 +0000

    Updated requirements

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'test-yes-two-hello-wireless'
 
 	# Mysql credentials
-	MYSQL_USER = os.environ.get('MYSQL_USER')
-	MYSQL_PASS = os.environ.get('MYSQL_PASS')
---
+++ launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#launcher.sh
+sleep 10
+cd /
+cd /home/pi/Projects/webapp
+venv/bin/python app.py
+cd /
